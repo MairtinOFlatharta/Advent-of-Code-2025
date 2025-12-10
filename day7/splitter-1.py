78efb0fcf06adca38cfb957143d3b5bf104a9bca
Máirtín Ó Flatharta <mairtinflatherty2014@gmail.com>
2025-12-10 10:45:19 +0000

    Add files for day 7

---
+++ day7/splitter-1.py
@@ -0,0 +1,53 @@
+def main():
+    raw = []
+    for line in read_input("input.txt"):
+        raw.append(line)
+    start = find_start(raw[0])
+    num_split = 0
+
+    for i in range(1, len(raw)-1):
+        if i == 1:
+            raw[i][start] = '|'
+        for j in range(1, len(raw[0])-1):
+            if is_beam(raw[i][j]):
+                if is_split(raw[i+1][j]):
+                    raw[i+1][j-1], raw[i+1][j+1] = ('|', '|')
+                    num_split += 1
+                if is_space(raw[i+1][j]):
+                    raw[i+1][j] = '|'
+    print(f"Number of times beam was split: {num_split}")
+
+
+def find_start(line):
+    for i in range(0, len(line)):
+        if line[i] == 'S':
+            return i
+    return None
+
+
+def is_split(char):
+    if char == '^':
+        return True
+    return False
+
+
+def is_beam(char):
+    if char == '|':
+        return True
+    return False
+
+
+def is_space(char):
+    if char == '.':
+        return True
+    return False
+
+
+def read_input(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            yield list(line.strip())
+
+
+if __name__ == "__main__":
+    main()
